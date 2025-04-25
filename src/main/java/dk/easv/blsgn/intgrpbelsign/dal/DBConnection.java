3234124e4f57924a7a481751ed0d21ce4298b96d
LuisCartucho <luipac01@easv365.dk>
2025-04-25 11:20:57 +0200

    dbc

--- src/main/java/dk/easv/blsgn/intgrpbelsign/dal/DBConnection.java
+++ src/main/java/dk/easv/blsgn/intgrpbelsign/dal/DBConnection.java
@@ -1,4 +1,20 @@
 package dk.easv.blsgn.intgrpbelsign.dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+
 public class DBConnection {
+    public Connection getConnection() throws SQLServerException {
+        SQLServerDataSource ds;
+        ds = new SQLServerDataSource();
+        ds.setDatabaseName("ETBar-INT-DB"); // make this unique as names are shared on server
+        ds.setUser("CSe2024b_e_18"); // Use your own username
+        ds.setPassword("CSe2024bE18!24"); // Use your own password
+        ds.setServerName("EASV-DB4");
+        ds.setPortNumber(1433);
+        ds.setTrustServerCertificate(true);
+        return ds.getConnection();
+    }
 }
